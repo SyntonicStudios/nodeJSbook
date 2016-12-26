1c0cbafc60b22e6c2efd804f80b967d4c83f5791
Brady Prigge <syntonicstudios@gmail.com>
2016-12-26 10:41:21 -0600

    Doing Chap7

--- chap7/notes/app.js
+++ chap7/notes/app.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
@@ -8,6 +9,8 @@ var bodyParser = require('body-parser');
 var index = require('./routes/index');
 // var users = require('./routes/users');
 var notes = require('./routes/notes');
+var FileStreamRotator = require('file-stream-rotator');
+var error = require('debug')('notes:error');
 
 var app = express();
 
@@ -15,9 +18,50 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+// Error Handling
+process.on('uncaughtException', function(err) {
+  error("I've crashed!!! - "+ (err.stack || err));
+});
+
+/*
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    // util.log(err.message);
+    res.status(err.status || 500);
+    error((err.status || 500) +' '+ error.message);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+*/
+
+// Logging Setup
+var accessLogStream;
+// console.log('process.env.REQUEST_LOG_FILE: ' + process.env.REQUEST_LOG_FILE);
+if (process.env.REQUEST_LOG_FILE) {
+    var logDirectory = path.dirname(process.env.REQUEST_LOG_FILE + '/.');
+    // console.log('logDirectory:' + logDirectory);
+    // console.log('fs.existsSync(logDirectory): ' + fs.existsSync(logDirectory));
+    fs.existsSync(logDirectory) || fs.mkdirSync(logDirectory);
+    accessLogStream = FileStreamRotator.getStream({
+      filename: process.env.REQUEST_LOG_FILE,
+      // var rotatingLogStream = require('file-stream-rotator').getStream({filename:"/tmp/test.log", frequency:"daily", verbose: false});
+      // filename: '/' + process.env.REQUEST_LOG_FILE + '/',
+      // filename: "/logs/test",
+      frequency: 'daily',
+      verbose: false
+    });
+}
+
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
+// app.use(logger('dev'));
+// app.use(logger(process.env.REQUEST_LOG_FORMAT || 'dev'));
+app.use(logger(process.env.REQUEST_LOG_FORMAT || 'dev', {
+    stream: accessLogStream ? accessLogStream : process.stdout
+}));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
@@ -50,6 +94,7 @@ app.use(function(req, res, next) {
 });
 
 // error handler
+/*
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
@@ -58,6 +103,17 @@ app.use(function(err, req, res, next) {
   // render the error page
   res.status(err.status || 500);
   res.render('error');
+});
+*/
+
+app.use(function(err, req, res, next) {
+  // util.log(err.message);
+  res.status(err.status || 500);
+  error((err.status || 500) +' '+ error.message);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
 });
 
 module.exports = app;
